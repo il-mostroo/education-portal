1c1cabcf16f6ea2cbb559c3454ee64c5f15e4a28
Mohammed Salmi <elmonstromail@gmail.com>
2024-09-29 19:28:31 +0100

    Add main menu functionality for mobile

---
+++ script.js
@@ -0,0 +1,16 @@
+//mobile menu effect:
+const openMenuIcon = document.querySelector(".open-menu");
+const mainNavigation = document.querySelector(".main-navigation");
+const closeMenuIcon = document.querySelector(".close-menu");
+
+openMenuIcon.addEventListener("click", () => {
+    openMenuIcon.classList.add("hide");
+    mainNavigation.classList.add("show-mobile-menu")
+    closeMenuIcon.classList.remove("hide");
+})
+
+closeMenuIcon.addEventListener("click", () => {
+    closeMenuIcon.classList.add("hide");
+    mainNavigation.classList.remove("show-mobile-menu");
+    openMenuIcon.classList.remove("hide");
+})
