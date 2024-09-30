9bfe3e27adf462049d20a4a71fbf0248239ca600
Mohammed Salmi <elmonstromail@gmail.com>
2024-09-30 01:09:29 +0100

    Add animated slide up effect for mobile menu

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const closeMenuIcon = document.querySelector(".close-menu");
 
 openMenuIcon.addEventListener("click", () => {
     openMenuIcon.classList.add("hide");
+    mainNavigation.classList.remove("hide-mobile-menu");
     mainNavigation.classList.add("show-mobile-menu");
     closeMenuIcon.classList.remove("hide");
 })
 
 closeMenuIcon.addEventListener("click", () => {
     closeMenuIcon.classList.add("hide");
+    mainNavigation.classList.add("hide-mobile-menu");
     mainNavigation.classList.remove("show-mobile-menu");
     openMenuIcon.classList.remove("hide");
 })
